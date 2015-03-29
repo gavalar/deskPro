d916e7d78bcb942179ad5df6c50583ee071b935e
Gavin Corbett <gavin.corbett@dowjones.com>
2015-03-29 20:48:06 +0100

    Setting up unit test and Class

---
+++ Calculator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Calculator
+ *
+ * @author Gavin Corbett <gav.corbett@gmail.com>
+ * @version $Id$
+ * @copyright Gavin Corbett
+ */
+class Calculator
+{
+}
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+require_once('Calculator.php');
+
+/**
+ * testCalculator
+ *
+ * @uses PHPUnit_Framework_TestCase
+ * @author Gavin Corbett <gav.corbett@gmail.com>
+ * @version $Id$
+ * @copyright Gavin Corbett
+ */
+class testCalculator extends PHPUnit_Framework_TestCase
+{
+}
